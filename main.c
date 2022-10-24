26e77b5262f4258d497cdf56af169703efdf0e5f
Artem Kan <kan.art.email@gmail.com>
2022-10-24 21:44:53 +0500

    change blinking pattern according to the device ID

--- main.c
+++ main.c
@@ -1,77 +1,48 @@
-/**
- * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- *
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example
- * @brief Blinky Example Application main file.
- *
- * This file contains the source code for a sample application to blink LEDs.
- *
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "boards.h"
 
-/**
- * @brief Function for application main entry.
- */
+#define BLINK_INTERVAL 300
+#define PAUSE_BETWEEN_BLINKS 1000
+
+void blink_led(int led_id)
+{
+    bsp_board_led_invert(led_id);
+    nrf_delay_ms(BLINK_INTERVAL);
+    bsp_board_led_invert(led_id);
+}
+
+void blink_led_many(int led_id, int n)
+{
+    if (n == 0)
+    {
+        nrf_delay_ms(PAUSE_BETWEEN_BLINKS);
+        return;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        blink_led(led_id);
+        nrf_delay_ms(BLINK_INTERVAL);
+    }
+
+    blink_led(led_id);
+    nrf_delay_ms(PAUSE_BETWEEN_BLINKS);
+}
+
+// Device ID is 7202
+const int BLINKS[LEDS_NUMBER] = { 7, 2, 0, 2 };
+
 int main(void)
 {
-    /* Configure board. */
     bsp_board_init(BSP_INIT_LEDS);
 
-    /* Toggle LEDs. */
     while (true)
     {
         for (int i = 0; i < LEDS_NUMBER; i++)
         {
-            bsp_board_led_invert(i);
-            nrf_delay_ms(500);
+	    blink_led_many(i, BLINKS[i]);
         }
     }
 }
-
-/**
- *@}
- **/
