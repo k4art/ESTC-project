7d00f8eeb19da5960bcee1f180e2ae7be942e6b2
Artem Kan <kan.art.email@gmail.com>
2022-11-21 16:02:02 +0500

    fix(utils.h): wrap CALL_IF_NOT_FULL with do { .. } while (0)

--- utils.h
+++ utils.h
@@ -1,7 +1,10 @@
 #ifndef __UTILS_H
 #define __UTILS_H
 
-#define CALL_IF_NOT_NULL(fn, ...) \
-  if (fn != NULL) fn(__VA_ARGS__)
+#define CALL_IF_NOT_NULL(fn, ...)    \
+  do                                 \
+  {                                  \
+    if (fn != NULL) fn(__VA_ARGS__); \
+  } while (0)                        \
 
 #endif
