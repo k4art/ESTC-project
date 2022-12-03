38bc7b6dbe5ef25343df702afdd7fc12ed697a39
Artem Kan <kan.art.email@gmail.com>
2022-12-03 18:08:22 +0500

    feat(color_picker): use of fvarstorage to keep last set color value

--- color_picker/color_picker.h
+++ color_picker/color_picker.h
@@ -6,6 +6,8 @@
 void color_picker_init(void);
 
 void color_picker_set_hsv(hsv_color_t hsv);
+void color_picker_restore_or_set_default_hsv(hsv_color_t hsv);
+
 void color_picker_enable(bsp_idx_t button_idx, bsp_idx_t rgb_led_idx, bsp_idx_t status_led_idx);
 
 #endif
